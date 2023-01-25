569b46619ee202276fbc92b8b9938c58f4e93661
Tanay Bhadra <tanaymbhadra@berkeley.edu>
2023-01-24 18:52:45 -0800

    Module 6 Changes:

--- beg-module-6/tests.py
+++ beg-module-6/tests.py
@@ -10,8 +10,8 @@ from IPython.display import Markdown, display
 class Tests(unittest.TestCase):
 
     def test_2a(self, f):
-        self.assertAlmostEqual(f(10), [2, 5])
-        self.assertAlmostEqual(f(104939402), [2,17, 31, 99563])
+        self.assertEquals(f(10), [2, 5])
+        self.assertEquals(f(104939402), [2,17, 31, 99563])
 
     def test_2b(self, f):
         self.assertEquals(f(20,5,2), 7)
@@ -19,8 +19,8 @@ class Tests(unittest.TestCase):
         self.assertEquals(f(40,5,2), 15)
         self.assertEquals(f(40,5,2), 15)
     def test_cat(self, f):
-        self.assertEquals(f(10), [1.0, 1.0, 2.0, 5.0, 14.0])
-        self.assertEquals(f(102434), [1.0, 1.0, 2.0, 5.0, 14.0, 42.0, 132.0, 429.0, 1430.0, 4862.0, 16796.0, 58786.0, 208012.0])
+        self.assertEquals(f(4), [1.0, 1.0, 2.0, 5.0, 14.0])
+        self.assertEquals(f(3), [1.0, 1.0, 2.0, 5.0])
 
 
     
